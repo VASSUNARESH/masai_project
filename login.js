9372b9509cfe637dc52e2b5d737ca86bbb615b14
unknown <Srikanth1314>
2022-09-24 12:29:43 +0530

    login logic

--- login.js
+++ login.js
@@ -1,3 +1,13 @@
 let reg_email="abc@gmail.com";
 
 let reg_pass="123456"
+
+let enter_email = "abc@gmail.com"
+
+let enter_pass = "123456";
+
+if(reg_email === enter_email && reg_pass === enter_pass){
+		
+		console.log("loggin succesfull");
+}   else   {
+		console.log("loggin failed");
